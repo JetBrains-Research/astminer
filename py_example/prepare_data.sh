dc51ead1330ec9ed68d42431d1089b45a6c24801
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-01-14 18:08:01 +0300

    Add data preparation as a step of py script

--- py_example/prepare_data.sh
+++ py_example/prepare_data.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/apache/kafka.git data/project1
 git clone https://github.com/apache/incubator-heron.git data/project2
 cd ../
-./gradlew run
+./gradlew processPyExample
 cd py_example
