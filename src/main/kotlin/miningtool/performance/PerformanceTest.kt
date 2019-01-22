d0e6d05e47874128e0ef1827ca775799ab05b319
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-01-22 19:00:26 +0300

    Create task for performance test

---
+++ src/main/kotlin/miningtool/performance/PerformanceTest.kt
@@ -0,0 +1,23 @@
+@file:JvmName("PerformanceTest")
+
+package miningtool.performance
+
+fun javaPerformanceTest() {
+    println("Java works well")
+}
+
+
+fun pythonPerformanceTest() {
+    println("Python works well")
+}
+
+
+fun cPerformanceTest() {
+    println("C works well")
+}
+
+fun main(args: Array<String>) {
+    javaPerformanceTest()
+    pythonPerformanceTest()
+    cPerformanceTest()
+}
