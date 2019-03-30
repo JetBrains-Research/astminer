cf66a3472b21ee389db440377d3a021741800caa
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-03-31 02:30:21 +0300

    Add joern wrapper

--- src/main/kotlin/astminer/parse/antlr/joern/CppParser.kt
+++ src/main/kotlin/astminer/parse/antlr/joern/CppParser.kt
@@ -1,22 +1,52 @@
 package astminer.parse.antlr.joern
 
-import me.vovak.antlr.parser.ModuleLexer
-import me.vovak.antlr.parser.ModuleParser
-import astminer.common.Parser
-import astminer.parse.antlr.SimpleNode
-import astminer.parse.antlr.convertAntlrTree
-import org.antlr.v4.runtime.CharStreams
-import org.antlr.v4.runtime.CommonTokenStream
-import java.io.InputStream
+import java.io.File
 
-class CppParser : Parser<SimpleNode> {
-    override fun parse(content: InputStream): SimpleNode? {
-        val lexer = ModuleLexer(CharStreams.fromStream(content))
-//        lexer.removeErrorListeners()
-        val tokens = CommonTokenStream(lexer)
-        val parser = ModuleParser(tokens)
-//        parser.removeErrorListeners()
-        val context = parser.code()
-        return convertAntlrTree(context, ModuleParser.ruleNames)
+fun parseJoernAst(nodesFile: File, edgesFile: File): JoernNode? {
+    val nodesRaw = nodesFile.readLines()
+    val nodesByIndex = nodesRaw.subList(1, nodesRaw.size).map { line ->
+        parseNodeLine(line)
+    }.toMap()
+
+    val edgesRaw = edgesFile.readLines()
+    if (edgesRaw.size == 1) {
+        return null
+    }
+    edgesRaw.subList(1, edgesRaw.size).forEach { line ->
+        parseEdgeLine(line)?.let { edge ->
+            val parentNode = nodesByIndex[edge.first]
+            val childNode = nodesByIndex[edge.second]
+            if (parentNode != null && childNode != null) {
+                parentNode.addChild(childNode)
+            } else {
+                throw NullPointerException("Edge references missing nodes at indices ${edge.first}, ${edge.second}")
+            }
+        }
+    }
+    for (node in nodesByIndex.values) {
+        if (node.getTypeLabel() == "File") {
+            return node
+        }
+    }
+    return null
+}
+
+private fun parseNodeLine(line: String): Pair<Int, JoernNode> {
+    val values = line.split('\t')
+    val key = values[1].toInt()
+    val nodeType = values[2]
+    val code = values[3]
+    return key to JoernNode(nodeType, code)
+}
+
+private fun parseEdgeLine(line: String): Pair<Int, Int>? {
+    val values = line.split('\t')
+    val start = values[0].toInt()
+    val end = values[1].toInt()
+    val typeLabel = values[2]
+    if (listOf("IS_AST_PARENT", "IS_FUNCTION_OF_AST", "IS_FILE_OF").contains(typeLabel)) {
+        return start to end
+    } else {
+        return null
     }
 }
