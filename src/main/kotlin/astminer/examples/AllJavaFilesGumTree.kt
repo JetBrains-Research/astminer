d3336c9a8110dfb58cbf656d1471bd6cda14c24d
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2019-06-05 15:05:09 +0200

    examples: extract file iteration function

--- src/main/kotlin/astminer/examples/AllJavaFilesGumTree.kt
+++ src/main/kotlin/astminer/examples/AllJavaFilesGumTree.kt
@@ -1,10 +1,10 @@
 package astminer.examples
 
-import astminer.paths.toPathContext
 import astminer.parse.java.GumTreeJavaParser
 import astminer.paths.PathMiner
 import astminer.paths.PathRetrievalSettings
 import astminer.paths.VocabularyPathStorage
+import astminer.paths.toPathContext
 import java.io.File
 
 //Retrieve paths from Java files, using a GumTree parser.
@@ -14,8 +14,8 @@ fun allJavaFilesGumTree() {
     val miner = PathMiner(PathRetrievalSettings(5, 5))
     val storage = VocabularyPathStorage()
 
-    File(folder).walkTopDown().filter { it.path.endsWith(".java") }.forEach { file ->
-        val node = GumTreeJavaParser().parse(file.inputStream()) ?: return@forEach
+    File(folder).forFilesWithSuffix(".java") { file ->
+        val node = GumTreeJavaParser().parse(file.inputStream()) ?: return@forFilesWithSuffix
         val paths = miner.retrievePaths(node)
 
         storage.store(paths.map { toPathContext(it) }, entityId = file.path)
--- src/main/kotlin/astminer/examples/AllPythonFiles.kt
+++ src/main/kotlin/astminer/examples/AllPythonFiles.kt
@@ -1,10 +1,10 @@
 package astminer.examples
 
-import astminer.paths.toPathContext
 import astminer.parse.antlr.python.PythonParser
 import astminer.paths.PathMiner
 import astminer.paths.PathRetrievalSettings
 import astminer.paths.VocabularyPathStorage
+import astminer.paths.toPathContext
 import java.io.File
 
 
@@ -14,12 +14,15 @@ fun allPythonFiles() {
     val miner = PathMiner(PathRetrievalSettings(5, 5))
     val storage = VocabularyPathStorage()
 
-    File(folder).walkTopDown().filter { it.path.endsWith(".py") }.forEach { file ->
-        val node = PythonParser().parse(file.inputStream()) ?: return@forEach
+    File(folder).forFilesWithSuffix(".py") { file ->
+        val node = PythonParser().parse(file.inputStream()) ?: return@forFilesWithSuffix
         val paths = miner.retrievePaths(node)
 
         storage.store(paths.map { toPathContext(it) }, entityId = file.path)
     }
+
+
+
 
     storage.save("out_examples/allPythonFiles")
 }
