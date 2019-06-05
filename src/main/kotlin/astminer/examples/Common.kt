ba34bc854fe0e7d441f14d4b9adb3289b9f0e518
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2019-06-05 15:05:34 +0200

    fix #20

--- src/main/kotlin/astminer/examples/Common.kt
+++ src/main/kotlin/astminer/examples/Common.kt
@@ -3,7 +3,7 @@ package astminer.examples
 import java.io.File
 
 fun iterateFiles(dir: File, condition: (File) -> Boolean, action: (File) -> Unit) {
-    dir.walkTopDown().filter { condition(it) }.forEach { action.invoke(it) }
+    dir.walkTopDown().filter { it.isFile && condition(it) }.forEach { action.invoke(it) }
 }
 
 fun File.forFilesWithSuffix(extension: String, action: (File) -> Unit) {
