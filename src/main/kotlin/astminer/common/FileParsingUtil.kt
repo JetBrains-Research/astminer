82712ae4b0cef079b17c796cedd1bde828019613
elena-lyulina <elena.lyulina@gmail.com>
2019-08-01 14:21:15 +0300

    Add some common functions for file parsing

---
+++ src/main/kotlin/astminer/common/FileParsingUtil.kt
@@ -0,0 +1,27 @@
+package astminer.common
+
+import me.vovak.antlr.parser.Java8Lexer
+import me.vovak.antlr.parser.Java8Parser
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import java.io.File
+
+fun numberOfLines(file: File) : Int {
+    return file.readLines().filter { it != "" }.size
+}
+
+fun changeExtensionTo(file: File, extension: String) {
+    file.renameTo(File("${file.parent}/${file.nameWithoutExtension}.$extension"))
+}
+
+fun addClassWrapper(file: File, className: String) {
+    file.writeText("class $className { \n ${file.readText()} \n }")
+}
+
+fun hasSyntaxErrors(file: File) : Boolean {
+    val lexer = Java8Lexer(CharStreams.fromStream(file.inputStream()))
+    val tokens = CommonTokenStream(lexer)
+    val parser = Java8Parser(tokens)
+    parser.compilationUnit()
+    return parser.numberOfSyntaxErrors != 0
+}
