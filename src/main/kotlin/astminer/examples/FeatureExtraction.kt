9f1513e062d2a8490ebf57d96f00b3d715b9bb0b
elena-lyulina <elena.lyulina@gmail.com>
2019-08-02 03:12:49 +0300

    Implement suggestions from a review

---
+++ src/main/kotlin/astminer/examples/FeatureExtraction.kt
@@ -0,0 +1,32 @@
+package astminer.examples
+
+import astminer.common.numberOfLines
+import astminer.featureextraction.*
+import astminer.parse.java.GumTreeJavaParser
+import java.io.File
+
+
+fun parseAndCollectFeatures() {
+    val parser = GumTreeJavaParser()
+    val features : List<TreeFeature<Any>> = listOf(Depth, NumberOfNodes, BranchingFactor, CompressiblePathLengths, Tokens, NodeTypes)
+
+    val folderInput = "./testData/featureextraction"
+    val folderOutput = "out_examples/featureextraction"
+
+    val storage = TreeFeatureValueStorage(",")
+    storage.storeFeatures(features)
+
+    File(folderInput).forFilesWithSuffix("java") { fileInput ->
+        val fileName = fileInput.name
+        val nol = numberOfLines(fileInput)
+
+        val tree = ParsedTree(parser.className(), parser.parse(fileInput.inputStream()) ?: return@forFilesWithSuffix, fileName, nol)
+        storage.storeParsedTree(tree)
+    }
+
+    storage.save(folderOutput)
+}
+
+fun main() {
+    parseAndCollectFeatures()
+}
