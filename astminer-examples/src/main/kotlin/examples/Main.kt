6befdf6cf90facfca323b36358eeb4cdfbf86931
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-09-11 12:55:07 +0300

    Add help note when script called without arguments

--- astminer-examples/src/main/kotlin/examples/Main.kt
+++ astminer-examples/src/main/kotlin/examples/Main.kt
@@ -2,11 +2,15 @@ package examples
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
-        throw Exception("Provide arguments to run the program!")
-    }
-    return when(args[0]) {
-        "preprocess" -> ProjectPreprocessor().main(args.sliceArray(1 until args.size))
-        "parse" -> ProjectParser().main(args.sliceArray(1 until args.size))
-        else -> throw Exception("The first argument should be either 'preprocess' or 'parse'")
+        println("""
+             You should specify the task as the first argument ("preprocess" or "parse").
+             For more information run `./cli.sh taskName --help`
+        """.trimIndent())
+    } else {
+        return when (args[0]) {
+            "preprocess" -> ProjectPreprocessor().main(args.sliceArray(1 until args.size))
+            "parse" -> ProjectParser().main(args.sliceArray(1 until args.size))
+            else -> throw Exception("The first argument should be task's name: either 'preprocess' or 'parse'")
+        }
     }
 }
