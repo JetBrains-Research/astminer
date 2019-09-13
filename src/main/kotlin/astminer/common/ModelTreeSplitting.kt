994ef78ce57db754789d39ff272336644d134665
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-09-13 14:16:51 +0300

    Method splitter interface

--- src/main/kotlin/astminer/common/ModelTreeSplitting.kt
+++ src/main/kotlin/astminer/common/ModelTreeSplitting.kt
@@ -4,3 +4,30 @@ package astminer.common
 interface TreeSplitter<T : Node> {
     fun split(root: T): Collection<T>
 }
+
+interface TreeMethodSplitter<T : Node> {
+    fun splitIntoMethods(root: T): Collection<MethodInfo<T>>
+}
+
+data class MethodInfo<T>(
+        val method: MethodNode<T>,
+        val enclosingClass: ClassNode<T>,
+        val methodParameters: List<ParameterNode<T>>
+)
+
+data class MethodNode<T>(
+        val root: T,
+        val returnTypeNode: T?,
+        val nameNode: T?
+)
+
+data class ClassNode<T>(
+        val root: T,
+        val nameNode: T?
+)
+
+data class ParameterNode<T>(
+        val root: T,
+        val returnTypeNode: T?,
+        val nameNode: T?
+)
