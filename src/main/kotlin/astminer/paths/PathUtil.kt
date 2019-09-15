8b6833898b8ebb582bd295e63c24d31754e933fb
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-09-15 22:09:22 +0300

    Do not drop ends of the path since they are not terminals anymore

--- src/main/kotlin/astminer/paths/PathUtil.kt
+++ src/main/kotlin/astminer/paths/PathUtil.kt
@@ -2,11 +2,10 @@ package astminer.paths
 
 import astminer.common.*
 
-fun toPathContext(path: ASTPath, getToken: (Node) -> String = { node -> node.getNormalizedToken()}): PathContext {
+fun toPathContext(path: ASTPath, getToken: (Node) -> String = { node -> node.getToken()}): PathContext {
     val startToken = getToken(path.upwardNodes.first())
     val endToken = getToken(path.downwardNodes.last())
-    val astNodes = path.upwardNodes
-            .takeLast(path.upwardNodes.size - 1).map { OrientedNodeType(it.getTypeLabel(), Direction.UP) } +
-            path.downwardNodes.take(path.downwardNodes.size - 1).map { OrientedNodeType(it.getTypeLabel(), Direction.DOWN) }
+    val astNodes = path.upwardNodes.map { OrientedNodeType(it.getTypeLabel(), Direction.UP) } +
+            path.downwardNodes.map { OrientedNodeType(it.getTypeLabel(), Direction.DOWN) }
     return PathContext(startToken, astNodes, endToken)
 }
