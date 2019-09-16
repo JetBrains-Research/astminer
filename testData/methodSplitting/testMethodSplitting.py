5c8959892ffb18397e03152822ce29672a8d611a
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-09-16 12:06:01 +0300

    Tests for python method splitting

---
+++ testData/methodSplitting/testMethodSplitting.py
@@ -0,0 +1,21 @@
+def funWithNoClass():
+    pass
+
+class Class1:
+
+    def funInClass1():
+        pass
+
+    class Class2:
+
+        def funInClass2():
+            pass
+
+def functionWithNoParameters():
+    pass
+
+def functionWithOneParameter(p1):
+    pass
+
+def functionWithThreeParameters(p1, p2 = 4, p3: int = 3):
+    pass
