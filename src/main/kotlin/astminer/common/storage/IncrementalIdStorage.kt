491711ef3a5a4cc2653469e0a23d75977584a4b3
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-09-16 12:48:39 +0300

    Add rank computation to incremental storage

--- src/main/kotlin/astminer/common/storage/IncrementalIdStorage.kt
+++ src/main/kotlin/astminer/common/storage/IncrementalIdStorage.kt
@@ -4,6 +4,7 @@ class IncrementalIdStorage<T> {
     private var keyCounter = 0L
     val idPerItem: MutableMap<T, Long> = HashMap()
     private val idCountMap: MutableMap<Long, Long> = HashMap()
+    private var idCountRanks: Map<Long, Long>? = null
 
     private fun putAndIncrementKey(item: T): Long {
         idPerItem[item] = ++keyCounter
@@ -26,5 +27,25 @@ class IncrementalIdStorage<T> {
 
     fun lookUpValue(id: Long): T? {
         return idPerItem.entries.firstOrNull { it.value == id }?.key
+    }
+
+    fun getKeyRank(item: T): Long {
+        if (idCountRanks == null) {
+            computeRanks()
+        }
+        return idCountRanks?.get(getId(item)) ?: 0
+    }
+
+    fun computeRanks() {
+        val sortedEntries = idCountMap.entries
+                .sortedBy { it.value }
+                .reversed()
+                .map { it.key }
+                .toList()
+        val idRankMap = mutableMapOf<Long, Long>()
+        for (i in sortedEntries.indices) {
+            idRankMap[sortedEntries[i]] = (i + 1).toLong()
+        }
+        idCountRanks = idRankMap
     }
 }
