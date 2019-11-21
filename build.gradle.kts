47f983a275346d148f895d762d7335f7dfc5fa9f
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-11-21 15:20:13 +0300

    Update version in gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import tanvd.kosogor.proxy.publishJar
 
 group = "io.github.vovak.astminer"
-version = "0.5.3"
+version = "0.5"
 
 plugins {
     id("java")
@@ -97,7 +97,7 @@ idea {
 
 publishJar {
     publication {
-        artifactId = "astminer-dev"
+        artifactId = "astminer"
     }
 
     bintray {
