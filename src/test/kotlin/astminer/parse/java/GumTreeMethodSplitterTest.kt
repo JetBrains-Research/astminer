11f99ef0422aa0d83c2899393637d4c7a6fd9525
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-12-02 15:20:53 +0300

    Update tests for gumtree method splitting

--- src/test/kotlin/astminer/parse/java/GumTreeMethodSplitterTest.kt
+++ src/test/kotlin/astminer/parse/java/GumTreeMethodSplitterTest.kt
@@ -1,49 +1,81 @@
 package astminer.parse.java
 
-import org.junit.Assert
+import astminer.common.model.MethodInfo
 import org.junit.Test
 import java.io.File
+import kotlin.test.assertEquals
 
 private fun createTree(filename: String): GumTreeJavaNode {
     val parser = GumTreeJavaParser()
     return parser.parse(File(filename).inputStream()) as GumTreeJavaNode
 }
 
-private fun createAndSplitTree(filename: String): Collection<GumTreeJavaNode> {
-    return GumTreeMethodSplitter().split(createTree(filename))
+private fun createAndSplitTree(filename: String): Collection<MethodInfo<GumTreeJavaNode>> {
+    return GumTreeMethodSplitter().splitIntoMethods(createTree(filename))
 }
 
 class GumTreeMethodSplitterTest {
     @Test
     fun testMethodExtraction1() {
-        val methodNodes = createAndSplitTree("testData/gumTreeMethodSplitter/1.java")
+        val methodInfos = createAndSplitTree("testData/gumTreeMethodSplitter/1.java")
 
-        Assert.assertEquals(1, methodNodes.size)
-        Assert.assertEquals(MethodInfo("SingleFunction", "fun", listOf("String[]", "int")), methodNodes.first().getMethodInfo())
+        assertEquals(1, methodInfos.size)
+        with(methodInfos.first()) {
+            assertEquals("fun", name())
+            assertEquals("void", returnType())
+            assertEquals("SingleFunction", enclosingElementName())
+            assertEquals(listOf("args", "param"), methodParameters.map { it.name() }.toList())
+            assertEquals(listOf("String[]", "int"), methodParameters.map { it.returnType() }.toList())
+        }
+
     }
 
     @Test
     fun testMethodExtraction2() {
-        val methodNodes = createAndSplitTree("testData/gumTreeMethodSplitter/2.java")
+        val methodInfos = createAndSplitTree("testData/gumTreeMethodSplitter/2.java")
 
-        Assert.assertEquals(1, methodNodes.size)
-        Assert.assertEquals(MethodInfo("InnerClass", "main", listOf("String[]")), methodNodes.first().getMethodInfo())
+        assertEquals(1, methodInfos.size)
+        with(methodInfos.first()) {
+            assertEquals("main", name())
+            assertEquals("void", returnType())
+            assertEquals("InnerClass", enclosingElementName())
+            assertEquals(listOf("args"), methodParameters.map { it.name() }.toList())
+            assertEquals(listOf("String[]"), methodParameters.map { it.returnType() }.toList())
+        }
     }
 
     @Test
     fun testMethodExtraction3() {
-        val methodNodes = createAndSplitTree("testData/gumTreeMethodSplitter/3.java")
+        val methodInfos = createAndSplitTree("testData/gumTreeMethodSplitter/3.java")
 
-        Assert.assertEquals(2, methodNodes.size)
-        Assert.assertTrue(methodNodes.map { it.getMethodInfo() }.contains(MethodInfo("InnerClass","main", listOf("String[]"))))
-        Assert.assertTrue(methodNodes.map { it.getMethodInfo() }.contains(MethodInfo("SingleMethodInnerClass", "fun", listOf("String[]", "int"))))
+        assertEquals(2, methodInfos.size)
+        with(methodInfos.first()) {
+            assertEquals("main", name())
+            assertEquals("void", returnType())
+            assertEquals("InnerClass", enclosingElementName())
+            assertEquals(listOf("args"), methodParameters.map { it.name() }.toList())
+            assertEquals(listOf("String[]"), methodParameters.map { it.returnType() }.toList())
+        }
+        with(methodInfos.last()) {
+            assertEquals("fun", name())
+            assertEquals("void", returnType())
+            assertEquals("SingleMethodInnerClass", enclosingElementName())
+            assertEquals(listOf("args", "param"), methodParameters.map { it.name() }.toList())
+            assertEquals(listOf("String[]", "int"), methodParameters.map { it.returnType() }.toList())
+        }
     }
 
     @Test
     fun testMethodExtraction4() {
-        val methodNodes = createAndSplitTree("testData/gumTreeMethodSplitter/4.java")
+        val methodInfos = createAndSplitTree("testData/gumTreeMethodSplitter/4.java")
 
-        Assert.assertEquals(1, methodNodes.size)
-        Assert.assertEquals(MethodInfo("SingleFunction", "fun", listOf("int", "int")), methodNodes.first().getMethodInfo())
+        assertEquals(1, methodInfos.size)
+        with(methodInfos.first()) {
+            assertEquals("fun", name())
+            assertEquals("int", returnType())
+            assertEquals("SingleFunction", enclosingElementName())
+            assertEquals(listOf("args", "param"), methodParameters.map { it.name() }.toList())
+            assertEquals(listOf("int", "SingleFunction"), methodParameters.map { it.returnType() }.toList())
+        }
     }
 }
