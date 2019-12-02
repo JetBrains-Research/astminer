b3d17c49157279a24b7e337ca8b1402764714946
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-12-02 15:24:52 +0300

    Set dev version to preliminary use in CLI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import tanvd.kosogor.proxy.publishJar
 
 group = "io.github.vovak.astminer"
-version = "0.5"
+version = "0.5.4"
 
 plugins {
     id("java")
@@ -97,7 +97,7 @@ idea {
 
 publishJar {
     publication {
-        artifactId = "astminer"
+        artifactId = "astminer-dev"
     }
 
     bintray {
