fe8ca6fc4e26ea062ebd93fa7fa8a71c4b23920a
Spirin Egor <spirin.egor@mail.ru>
2020-03-10 10:21:47 +0300

    replace recursive call by SELF token

--- astminer-cli/src/main/kotlin/cli/Granularity.kt
+++ astminer-cli/src/main/kotlin/cli/Granularity.kt
@@ -90,6 +90,11 @@ class MethodGranularity(override val splitTokens: Boolean,
 
             methodRoot.preOrder().forEach { node -> processNodeToken(node, splitTokens) }
             if (hideMethodNames) {
+                methodRoot.preOrder().forEach { node ->
+                    if (node.getToken() == methodName) {
+                        node.setNormalizedToken("<SELF>")
+                    }
+                }
                 methodNameNode.setNormalizedToken("METHOD_NAME")
             }
             if (splitTokens) {
