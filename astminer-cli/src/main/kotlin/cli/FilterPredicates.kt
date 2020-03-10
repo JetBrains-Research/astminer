4a80fb485971f036379240fcbbdb35b58d3bdc26
Spirin Egor <spirin.egor@mail.ru>
2020-03-10 12:55:49 +0300

    add filters based on tree statistic

--- astminer-cli/src/main/kotlin/cli/FilterPredicates.kt
+++ astminer-cli/src/main/kotlin/cli/FilterPredicates.kt
@@ -1,7 +1,10 @@
 package cli
 
+import astminer.common.getNormalizedToken
 import astminer.common.model.MethodInfo
 import astminer.common.model.Node
+import astminer.common.preOrder
+import astminer.common.splitToSubtokens
 
 abstract class MethodFilterPredicate {
     open fun isFiltered(methodInfo: MethodInfo<out Node>): Boolean = false
@@ -46,5 +49,42 @@ class ConstructorFilterPredicate : MethodFilterPredicate() {
 
     override fun isFiltered(methodInfo: MethodInfo<out Node>): Boolean {
         return methodInfo.name() != methodInfo.enclosingElementName()
+    }
+}
+
+class MethodNameLengthFilterPredicate(private val maxLength: Int) : MethodFilterPredicate() {
+    override fun isFiltered(methodInfo: MethodInfo<out Node>): Boolean {
+        if (maxLength == -1) {
+            return true
+        }
+        val nameNode = methodInfo.method.nameNode
+        return if (nameNode != null) {
+            splitToSubtokens(nameNode.getToken()).size <= maxLength
+        } else {
+            false
+        }
+    }
+}
+
+class TokenLengthFilterPredicate(private val maxLength: Int) : MethodFilterPredicate() {
+    override fun isFiltered(methodInfo: MethodInfo<out Node>): Boolean {
+        if (maxLength == -1) {
+            return true
+        }
+        methodInfo.method.root.preOrder().forEach { node ->
+            if (splitToSubtokens(node.getToken()).size > maxLength) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class TreeSizeFilterPredicate(private val maxSize: Int) : MethodFilterPredicate() {
+    override fun isFiltered(methodInfo: MethodInfo<out Node>): Boolean {
+        if (maxSize == -1) {
+            return true
+        }
+        return methodInfo.method.root.preOrder().size <= maxSize
     }
 }
