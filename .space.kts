5978ad918a1cc2505740a18ca7d69b3bb44cbe21
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2021-04-29 18:51:53 +0300

    add secrets to env

--- .space.kts
+++ .space.kts
@@ -6,6 +6,9 @@ job("Release") {
     }
 
     container("openjdk:11") {
+        env["PUBLISH_USER"] = Secrets("PUBLISH_USER")
+        env["PUBLISH_PASSWORD"] = Secrets("PUBLISH_PASSWORD")
+        
         shellScript {
             content = """
               ./gradlew publish    
