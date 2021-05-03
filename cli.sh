731976aba85a893488b9c6a34dd691f985ef6cf9
Egor Spirin <spirin.egor@mail.ru>
2021-05-03 19:07:37 +0300

    cli.sh check if docker image exist

--- cli.sh
+++ cli.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-java -jar build/shadow/lib-*.jar "$@"
+IMAGE_NAME="voudy/astminer"
+SHADOW_JAR_PATH="build/shadow/astminer.jar"
+
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+  echo "Can't find docker image, will compile from sources"
+  ./gradlew shadowJar
+  java -jar $SHADOW_JAR_PATH "$@"
+else
+  echo "Run ASTMiner inside docker"
+  docker run --rm voudy/astminer "$@"
+fi
+
