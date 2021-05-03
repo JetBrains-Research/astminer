658cc70c2ac2a20dd60b7966f1133a455a3a5ea5
illided <utkin.ilya.nickolaevich@gmail.com>
2021-05-03 13:08:07 +0300

    small refactoring

--- src/main/kotlin/astminer/parse/gumtree/java/GumTreeJavaMethodSplitter.kt
+++ src/main/kotlin/astminer/parse/gumtree/java/GumTreeJavaMethodSplitter.kt
@@ -5,15 +5,10 @@ import astminer.common.preOrder
 import astminer.parse.gumtree.GumTreeNode
 
 class GumTreeJavaMethodSplitter : TreeMethodSplitter<GumTreeNode> {
-
-    companion object {
-        private object TypeLabels {
-            const val methodDeclaration = "MethodDeclaration"
-        }
-    }
+    private val methodDeclaration = "MethodDeclaration"
 
     override fun splitIntoMethods(root: GumTreeNode): Collection<FunctionInfo<GumTreeNode>> {
-        val methodRoots = root.preOrder().filter { it.getTypeLabel() == TypeLabels.methodDeclaration }
-        return methodRoots.map { GumTreeJavaFunctionInfo(it as GumTreeNode)}
+        val methodRoots = root.preOrder().filter { it.getTypeLabel() == methodDeclaration }
+        return methodRoots.map { GumTreeJavaFunctionInfo(it as GumTreeNode) }
     }
 }
