902ce183fe4e1c6fb697e0ab6fafe2f7f8953a66
illided <utkin.ilya.nickolaevich@gmail.com>
2021-05-05 16:30:38 +0300

    gumtreefunctioninfo added

---
+++ src/main/kotlin/astminer/parse/gumtree/python/GumTreeFunctionInfo.kt
@@ -0,0 +1,83 @@
+package astminer.parse.gumtree.python
+
+import astminer.common.model.EnclosingElement
+import astminer.common.model.EnclosingElementType
+import astminer.common.model.FunctionInfo
+import astminer.common.model.MethodInfoParameter
+import astminer.parse.findEnclosingElementBy
+import astminer.parse.gumtree.GumTreeNode
+
+class GumTreeFunctionInfo(override val root: GumTreeNode) : FunctionInfo<GumTreeNode> {
+    companion object {
+        private object TypeLabels {
+            const val classDefinition = "ClassDef"
+            const val functionDefinition = "FunctionDef"
+            const val asyncFunctionDefinition = "AsyncFunctionDef"
+            const val nameLoad = "Name_Load"
+            const val posOnlyArgs = "posonlyargs"
+            const val kwOnlyArgs = "kwonlyargs"
+            const val arguments = "arguments"
+            const val vararg = "vararg"
+            const val kwarg = "kwarg"
+            const val args = "args"
+            const val arg = "arg"
+
+            const val body = "body"
+            const val returnTypeLabel = "Return"
+            const val passTypeLabel = "Pass"
+            const val constantType = "Constant-"
+
+            val methodDefinitions = listOf(functionDefinition, asyncFunctionDefinition)
+            val funcArgsTypesNodes = listOf(args, posOnlyArgs, kwOnlyArgs)
+        }
+    }
+
+    override val nameNode: GumTreeNode = root
+    override val parameters: List<MethodInfoParameter> = collectParameters()
+    override val enclosingElement: EnclosingElement<GumTreeNode>? = collectEnclosingClass()
+
+    private fun getElementType(node: GumTreeNode): GumTreeNode? {
+        if (node.getTypeLabel() == TypeLabels.arg) {
+            return node.getChildOfType(TypeLabels.nameLoad)
+        }
+        if (TypeLabels.methodDefinitions.contains(node.getTypeLabel())) {
+            return node.getChildOfType(TypeLabels.body)?.getChildOfType(TypeLabels.returnTypeLabel)?.let {
+                it.getChildren().firstOrNull { child ->
+                    child.getTypeLabel().startsWith(TypeLabels.constantType)
+                }
+            }
+        }
+        return null
+    }
+
+    private fun collectEnclosingClass(): EnclosingElement<GumTreeNode>? {
+        val enclosing = findEnclosingClass() ?: return null
+        return EnclosingElement(
+            type = EnclosingElementType.Class,
+            name = enclosing.getToken(),
+            root = enclosing
+        )
+    }
+
+    private fun findEnclosingClass(): GumTreeNode? {
+        return root.findEnclosingElementBy { it.getTypeLabel() == TypeLabels.classDefinition }
+    }
+
+    private fun collectParameters(): List<MethodInfoParameter> {
+        val arguments = root.getChildrenOfType(TypeLabels.arguments).flatMap { it.getChildren() }
+        val params = arguments.flatMap { node ->
+            when (node.getTypeLabel()) {
+                in TypeLabels.funcArgsTypesNodes -> node.getChildren().flatMap { it.getChildren() }
+                    .filter { it.getTypeLabel() == TypeLabels.arg }
+                TypeLabels.vararg, TypeLabels.kwarg -> listOf(node)
+                else -> emptyList()
+            }
+        }
+        return params.map { node->
+            MethodInfoParameter(
+                name = node.getToken(),
+                type = getElementType(node)?.getToken()
+            )
+        }
+    }
+}
