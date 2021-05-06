b2e87a66949abb8c82152cf60f0784d1e1ebc009
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2021-05-06 08:43:42 +0300

    fix cli script: do not run gradle every time, change wording

--- cli.sh
+++ cli.sh
@@ -4,11 +4,14 @@ IMAGE_NAME="voudy/astminer"
 SHADOW_JAR_PATH="build/shadow/astminer.jar"
 
 if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
-  echo "Can't find docker image, will compile from sources"
-  ./gradlew shadowJar
+  echo "Docker image not found, will use $SHADOW_JAR_PATH";
+  if ! [[ -f "$SHADOW_JAR_PATH" ]]; then
+    echo "$SHADOW_JAR_PATH not found, building" 
+    ./gradlew shadowJar
+  fi
   java -jar $SHADOW_JAR_PATH "$@"
 else
-  echo "Run ASTMiner inside docker"
+  echo "Running astminer in docker"
   docker run --rm voudy/astminer "$@"
 fi
 
