a0d7f6f7dd2430232a4cc8903bd96652047dc1d6
furetur <furetur@gmail.com>
2021-05-20 14:20:33 +0500

    migrated old tests

---
+++ src/test/kotlin/astminer/pipeline/Code2VecExtractionTest.kt
@@ -0,0 +1,33 @@
+package astminer.pipeline
+
+import astminer.config.Code2VecPathStorageConfig
+import astminer.config.FilePathExtractorConfig
+import astminer.config.FilePipelineConfig
+import astminer.config.ParserConfig
+import astminer.pipeline.util.verifyPathContextExtraction
+import org.junit.Test
+import java.io.File
+import java.nio.file.Files
+
+internal class Code2VecExtractionTest {
+    private val testDataDir = File("src/test/resources")
+
+    // TODO: this test should probably be moved to Code2VecPathStorage
+    @Test
+    fun `test code2vec path extraction from files generates correct folders and files`() {
+        val extractedDataDir = Files.createTempDirectory("extractedData")
+
+        val languages = listOf("java", "py")
+
+        val config = FilePipelineConfig(
+            inputDir = testDataDir.path,
+            outputDir = extractedDataDir.toAbsolutePath().toString(),
+            parserConfig = ParserConfig("antlr", languages),
+            problemConfig = FilePathExtractorConfig(),
+            storageConfig = Code2VecPathStorageConfig(8, 3)
+        )
+        Pipeline(config).run()
+
+        verifyPathContextExtraction(extractedDataDir.toFile(), languages, false)
+    }
+}
