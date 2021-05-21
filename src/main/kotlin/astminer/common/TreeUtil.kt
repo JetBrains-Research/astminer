f91a401c0bfcd63eafd4ab1814c50b55ef369722
illided <utkin.ilya.nickolaevich@gmail.com>
2021-05-21 12:58:45 +0300

    comment removed

--- src/main/kotlin/astminer/common/TreeUtil.kt
+++ src/main/kotlin/astminer/common/TreeUtil.kt
@@ -10,31 +10,16 @@ fun Node.postOrderIterator(): Iterator<Node> {
     return postOrder().listIterator()
 }
 
-/*fun Node.preOrderIterator(): Iterator<Node> {
-    return preOrder().listIterator()
-}*/
-
 fun doTraversePostOrder(node: Node, resultList: MutableList<Node>) {
     node.children.forEach { doTraversePostOrder(it, resultList) }
     resultList.add(node)
 }
-
-/*fun doTraversePreOrder(node: Node, resultList: MutableList<Node>) {
-    resultList.add(node)
-    node.children.forEach { doTraversePreOrder(it, resultList) }
-}*/
 
 fun Node.postOrder(): List<Node> {
     val result: MutableList<Node> = ArrayList()
     doTraversePostOrder(this, result)
     return result
 }
-
-/*fun Node.preOrder(): List<Node> {
-    val result: MutableList<Node> = ArrayList()
-    doTraversePreOrder(this, result)
-    return result
-}*/
 
 const val DEFAULT_TOKEN = "EMPTY_TOKEN"
 const val TECHNICAL_TOKEN_KEY = "technical_token"
