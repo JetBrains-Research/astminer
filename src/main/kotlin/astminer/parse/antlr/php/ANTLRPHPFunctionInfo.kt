9de47564ed9bdcc580b3ecc73e8250111af262e7
illided <utkin.ilya.nickolaevich@gmail.com>
2021-05-29 16:55:23 +0300

    parameters extracting added

--- src/main/kotlin/astminer/parse/antlr/php/ANTLRPHPFunctionInfo.kt
+++ src/main/kotlin/astminer/parse/antlr/php/ANTLRPHPFunctionInfo.kt
@@ -1,11 +1,50 @@
 package astminer.parse.antlr.php
 
+import astminer.common.DEFAULT_TOKEN
 import astminer.common.model.FunctionInfo
-import astminer.parse.antlr.AntlrNode
+import astminer.common.model.FunctionInfoParameter
+import astminer.parse.antlr.*
 
 abstract class ANTLRPHPFunctionInfo(
-    override val root: AntlrNode
+    final override val root: AntlrNode
 ) : FunctionInfo<AntlrNode> {
+    override val parameters: List<FunctionInfoParameter> = collectParameters()
+
+    override val returnType = getElementType(root)
+
+    companion object {
+        const val PARAMETERS = "formalParameterList"
+        const val ONE_PARAMETER = "formalParameter"
+        const val TYPE = "typeHint"
+        const val PARAMETER_NAME = "VarName"
+    }
+
+    private fun collectParameters(): List<FunctionInfoParameter> {
+        val parameterList = root.getChildOfType(PARAMETERS) ?: return emptyList()
+        return parameterList.getItOrChildrenOfType(ONE_PARAMETER).map {
+            assembleParameter(it)
+        }
+    }
+
+    private fun assembleParameter(parameterNode: AntlrNode): FunctionInfoParameter {
+        return FunctionInfoParameter(
+            name = getParameterName(parameterNode),
+            type = getElementType(parameterNode)
+        )
+    }
+
+    private fun getParameterName(parameterNode: AntlrNode): String {
+        if (parameterNode.hasLastLabel(PARAMETER_NAME))
+            return parameterNode.originalToken ?: return DEFAULT_TOKEN
+        return parameterNode.children
+            .filter { !it.hasFirstLabel(TYPE) }
+            .map { it.originalToken }
+            .joinToString("")
+    }
+
+    private fun getElementType(element: AntlrNode): String? {
+        return element.getChildOfType(TYPE)?.originalToken
+    }
 }
 
 class ArrowPhpFunctionInfo(root: AntlrNode) : ANTLRPHPFunctionInfo(root) {
