9ef6b1b4bc82142e2a56b8a764897829460471db
illided <utkin.ilya.nickolaevich@gmail.com>
2021-05-29 16:55:07 +0300

    more tests added

--- src/test/kotlin/astminer/parse/antlr/php/ANTLRPHPMethodSplitterTest.kt
+++ src/test/kotlin/astminer/parse/antlr/php/ANTLRPHPMethodSplitterTest.kt
@@ -1,9 +1,11 @@
 package astminer.parse.antlr.php
 
+import astminer.common.model.EnclosingElementType
 import org.junit.Test
 import kotlin.test.BeforeTest
 import astminer.common.model.FunctionInfo
 import astminer.parse.antlr.AntlrNode
+import astminer.parse.antlr.javascript.JavaScriptFunctionSplitterTest
 import java.io.File
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -30,4 +32,31 @@ internal class ANTLRPHPMethodSplitterTest {
         assertEquals(N_METHODS, functionInfos.size, "Test file contains $N_METHODS methods")
     }
 
+    @Test
+    fun testValidMethodInfo() {
+        fun EnclosingElementType.getEnclosingElementType(): String {
+            return when (this) {
+                EnclosingElementType.Function -> "function"
+                EnclosingElementType.Class -> "class"
+                EnclosingElementType.Method -> "method"
+                EnclosingElementType.VariableDeclaration -> "variable"
+                else -> ""
+            }
+        }
+
+        fun FunctionInfo<AntlrNode>.getJsonInfo(): String {
+            return "info : {" +
+                    "name : ${name}, " +
+                    "args : ${parameters.joinToString(", ") { "${it.type} ${it.name}" }}, " +
+                    "enclosing element : ${enclosingElement?.type?.getEnclosingElementType()}, " +
+                    "}"
+        }
+
+        val actualJsonInfos = functionInfos.map { it.getJsonInfo() }.sorted()
+
+        val text = File(JavaScriptFunctionSplitterTest.testFilePath).readText()
+        val expectedJsonInfos = Regex("info : \\{.*\\}").findAll(text).toList().map { it.value }.sorted()
+
+        assertEquals(expectedJsonInfos, actualJsonInfos)
+    }
 }
