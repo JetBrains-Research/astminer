e75d3393424a0b25bd25691668d2b98fce673cde
illided <utkin.ilya.nickolaevich@gmail.com>
2021-05-30 15:16:54 +0300

    raw algorithm for enclosing extraction added

--- src/main/kotlin/astminer/parse/antlr/php/ANTLRPHPFunctionInfo.kt
+++ src/main/kotlin/astminer/parse/antlr/php/ANTLRPHPFunctionInfo.kt
@@ -1,13 +1,15 @@
 package astminer.parse.antlr.php
 
 import astminer.common.model.EnclosingElement
+import astminer.common.model.EnclosingElementType
 import astminer.common.model.FunctionInfo
 import astminer.common.model.FunctionInfoParameter
 import astminer.parse.antlr.*
+import astminer.parse.findEnclosingElementBy
 
 class ANTLRPHPFunctionInfo(override val root: AntlrNode) : FunctionInfo<AntlrNode> {
     override val returnType = getElementType(root)
-    override val nameNode: AntlrNode? = root.getChildOfType(FUNCTION)
+    override val nameNode: AntlrNode? = root.getChildOfType(FUNCTION_NAME)
 
     override val parameters: List<FunctionInfoParameter> = collectParameters()
     override val enclosingElement: EnclosingElement<AntlrNode>? = collectEnclosingElement()
@@ -19,11 +21,16 @@ class ANTLRPHPFunctionInfo(override val root: AntlrNode) : FunctionInfo<AntlrNod
         const val PARAMETER_NAME = "VarName"
         const val CLASS_MEMBER = "classStatement"
         const val FUNCTION = "functionDeclaration"
-        const val FUNCTION_NAME = "Identifier"
+        const val FUNCTION_NAME = "identifier"
         const val LAMBDA_DECLARATION = "lambdaFunctionExpr"
         const val CLASS_DECLARATION = "classDeclaration"
         const val VAR_DECLARATION = "variableInitializer"
         const val ELLIPSIS = "Ellipsis"
+        const val EXPRESSION = "expression"
+        const val ASSIGN_OP = "assignmentOperator"
+        const val LAMBDA_TOKEN = "LambdaFn"
+        const val FUNCTION_TOKEN = "Function_"
+        val POSSIBLE_FUNCTION_TOKENS = listOf(LAMBDA_TOKEN, FUNCTION_TOKEN)
     }
 
     private fun collectParameters(): List<FunctionInfoParameter> {
@@ -57,7 +64,7 @@ class ANTLRPHPFunctionInfo(override val root: AntlrNode) : FunctionInfo<AntlrNod
     }
 
     private fun getParameterName(parameterNode: AntlrNode): String {
-        // "..." in php equivalent to *args in python
+        // "...$args" in php equivalent to *args in python
         val isSplattedArg = parameterNode.getChildOfType(ELLIPSIS) != null
 
         if (parameterNode.hasLastLabel(PARAMETER_NAME)) return parameterNode.originalToken
@@ -76,6 +83,44 @@ class ANTLRPHPFunctionInfo(override val root: AntlrNode) : FunctionInfo<AntlrNod
     }
 
     private fun collectEnclosingElement(): EnclosingElement<AntlrNode>? {
-        return null
+        val enclosing = root.findEnclosingElementBy { it.isPossibleEnclosing() } ?: return null
+        return try {
+            EnclosingElement(
+                root = enclosing,
+                name = getEnclosingElementName(enclosing),
+                type = getEnclosingType(enclosing)
+            )
+        } catch (e: IllegalStateException) {
+            null
+        }
     }
+
+    private fun getEnclosingType(enclosing: AntlrNode): EnclosingElementType {
+        return when {
+            enclosing.isMethod() -> EnclosingElementType.Method
+            enclosing.isFunction() -> EnclosingElementType.Function
+            enclosing.isClass() -> EnclosingElementType.Class
+            enclosing.isAssignExpression() -> EnclosingElementType.VariableDeclaration
+            else -> throw IllegalStateException("No type can be associated")
+        }
+    }
+
+    private fun getEnclosingElementName(enclosing: AntlrNode) : String?{
+        return enclosing.getChildOfType(FUNCTION_NAME)?.originalToken
+//        return when {
+//            enclosing.isMethod() || enclosing.isFunction() -> enclosing.getChildOfType(FUNCTION_NAME)?.originalToken
+//            else -> throw IllegalStateException("No type can be associated")
+//        }
+    }
+
+    // No check for method because method is a function
+    private fun AntlrNode.isPossibleEnclosing() = isFunction() || isClass() || isAssignExpression()
+
+    private fun AntlrNode.isMethod() = isFunction() && (hasFirstLabel(CLASS_MEMBER))
+
+    private fun AntlrNode.isFunction() = getChildOfType(LAMBDA_TOKEN) != null || getChildOfType(FUNCTION_TOKEN) != null
+
+    private fun AntlrNode.isAssignExpression() = hasFirstLabel(EXPRESSION) && (getChildOfType(ASSIGN_OP) != null)
+
+    private fun AntlrNode.isClass(): Boolean = hasLastLabel(CLASS_DECLARATION)
 }
