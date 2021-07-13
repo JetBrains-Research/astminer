a92eeb4846630280100d13757038e007f6a1c4f6
illided <utkin.ilya.nickolaevich@gmail.com>
2021-07-13 17:46:58 +0300

    small refactoring

--- src/main/kotlin/astminer/parse/javaparser/JavaParserNode.kt
+++ src/main/kotlin/astminer/parse/javaparser/JavaParserNode.kt
@@ -6,25 +6,27 @@ import com.github.javaparser.ast.expr.BinaryExpr
 import com.github.javaparser.ast.expr.UnaryExpr
 import com.github.javaparser.ast.Node as JPNode
 
+/* Be aware that JPNode is just an alias for Node from javaparser*/
 class JavaParserNode(jpNode: JPNode, override val parent: Node?) : Node() {
-    override val children: List<Node> = jpNode.childNodes.map { subTree -> JavaParserNode(subTree, this) }
     override val originalToken: String? = getValue(jpNode)
-
+    override val children: MutableList<Node> = run {
+        jpNode.childNodes.map { subTree -> JavaParserNode(subTree, this) }.toMutableList()
+    }
     override val typeLabel: String = run {
         val rawType = getRawType(jpNode)
-        return@run SHORTEN_VALUES.getOrDefault(rawType, rawType)
+        SHORTEN_VALUES.getOrDefault(rawType, rawType)
     }
 
     override fun removeChildrenOfType(typeLabel: String) {
-        TODO("Not yet implemented")
+        children.removeIf { it.typeLabel == typeLabel }
     }
 
+    /* For some reason code2seq JavaExtractor also checks for boxed type
+       and sets its type to PrimitiveType
+       which is not necessary since javaclass.simpleName
+       will be PrimitiveType nonetheless*/
     private fun getRawType(jpNode: JPNode): String {
         val type = jpNode.javaClass.simpleName
-        /* For some reason code2seq JavaExtractor also checks for boxed type
-            and sets its type to PrimitiveType
-            which is not necessary since javaclass.simpleName
-            will be PrimitiveType nonetheless*/
         val operator = when (jpNode) {
             is UnaryExpr -> ":${jpNode.operator}"
             is BinaryExpr -> ":${jpNode.operator}"
