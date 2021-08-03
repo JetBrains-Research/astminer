e4e44a3e630ec178eba9729c8373fb4d3f712c0f
illided <utkin.ilya.nickolaevich@gmail.com>
2021-08-03 19:28:34 +0300

    code style issues fixed

--- src/test/kotlin/astminer/pipeline/PipelineAsyncStressTest.kt
+++ src/test/kotlin/astminer/pipeline/PipelineAsyncStressTest.kt
@@ -2,8 +2,8 @@ package astminer.pipeline
 
 import astminer.config.*
 import org.junit.AfterClass
-import org.junit.Test
 import org.junit.BeforeClass
+import org.junit.Test
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
@@ -11,6 +11,35 @@ import kotlin.test.AfterTest
 import kotlin.test.assertEquals
 
 class PipelineAsyncStressTest {
+    @AfterTest
+    fun deleteOutput() {
+        tempOutputDir.deleteRecursively()
+    }
+
+    @Test
+    fun jsonStorageTest() {
+        val config = PipelineConfig(
+            inputDir = tempInputDir.path,
+            outputDir = tempOutputDir.path,
+            parser = ParserConfig(
+                name = ParserType.Antlr,
+                languages = listOf(FileExtension.Java)
+            ),
+            filters = listOf(),
+            labelExtractor = FunctionNameExtractorConfig(),
+            storage = JsonAstStorageConfig()
+        )
+        Pipeline(config).run()
+        assertEquals((numOfFiles * numOfMethods).toLong(), countLines("${tempOutputDir.path}/java/asts.jsonl"))
+    }
+
+    private fun countLines(filePath: String): Long {
+        val reader = BufferedReader(FileReader(filePath))
+        var numOfLines = 0L
+        while (reader.readLine() != null) { numOfLines++ }
+        return numOfLines
+    }
+
     companion object {
         private const val numOfFiles = 3000
         private const val numOfMethods = 100
@@ -24,7 +53,9 @@ class PipelineAsyncStressTest {
             repeat(numOfFiles) { index ->
                 val newFile = File.createTempFile("someFile", ".java", tempInputDir)
                 newFile.writeText("class someClass$index {\n")
-                repeat(numOfMethods) {newFile.appendText("public void someMethod${it + index * numOfMethods}() {} \n")}
+                repeat(numOfMethods) {
+                    newFile.appendText("public void someMethod${it + index * numOfMethods}() {} \n")
+                }
                 newFile.appendText("}")
             }
         }
@@ -34,34 +65,5 @@ class PipelineAsyncStressTest {
         fun tearDown() {
             tempInputDir.deleteRecursively()
         }
-
-    }
-
-    @AfterTest
-    fun deleteOutput() {
-        tempOutputDir.deleteRecursively()
-    }
-
-    @Test
-    fun jsonStorageTest() {
-        val config = PipelineConfig(
-            inputDir = tempInputDir.path,
-            outputDir = tempOutputDir.path,
-            parser = ParserConfig(
-                name = ParserType.Antlr,
-                languages = listOf(FileExtension.Java)),
-            filters = listOf(),
-            labelExtractor = FunctionNameExtractorConfig(),
-            storage = JsonAstStorageConfig()
-        )
-        Pipeline(config).run()
-        assertEquals((numOfFiles * numOfMethods).toLong(), countLines("${tempOutputDir.path}/java/asts.jsonl"))
-    }
-
-    private fun countLines(filePath: String): Long {
-        val reader = BufferedReader(FileReader(filePath))
-        var numOfLines = 0L
-        while (reader.readLine() != null) {numOfLines++}
-        return numOfLines
     }
 }
