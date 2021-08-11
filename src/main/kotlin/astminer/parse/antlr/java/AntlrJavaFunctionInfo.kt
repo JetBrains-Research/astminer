7419bad03055cd2f0b41fc820f23e30337d4909f
illided <utkin.ilya.nickolaevich@gmail.com>
2021-08-11 12:45:55 +0300

    body property added in java gumtree and java antlr

--- src/main/kotlin/astminer/parse/antlr/java/AntlrJavaFunctionInfo.kt
+++ src/main/kotlin/astminer/parse/antlr/java/AntlrJavaFunctionInfo.kt
@@ -18,6 +18,8 @@ class AntlrJavaFunctionInfo(override val root: AntlrNode, override val filePath:
             null
         }
 
+    override val body: AntlrNode? = root.children.find { it.hasFirstLabel(METHOD_BODY_NODE) }
+
     private fun collectNameNode(): AntlrNode? = root.getChildOfType(METHOD_NAME_NODE)
 
     private fun collectReturnType(): String? {
@@ -60,6 +62,7 @@ class AntlrJavaFunctionInfo(override val root: AntlrNode, override val filePath:
     companion object {
         private const val METHOD_RETURN_TYPE_NODE = "typeTypeOrVoid"
         private const val METHOD_NAME_NODE = "IDENTIFIER"
+        private const val METHOD_BODY_NODE = "methodBody"
 
         private const val CLASS_DECLARATION_NODE = "classDeclaration"
         private const val CLASS_NAME_NODE = "IDENTIFIER"
