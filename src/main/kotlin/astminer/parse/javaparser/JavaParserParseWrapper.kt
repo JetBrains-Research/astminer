4a0450cb93f35467a670160d2d654df77ecb3c25
illided <utkin.ilya.nickolaevich@gmail.com>
2021-08-13 13:20:30 +0300

    static java parser replaced with simple javaparser

--- src/main/kotlin/astminer/parse/javaparser/JavaParserParseWrapper.kt
+++ src/main/kotlin/astminer/parse/javaparser/JavaParserParseWrapper.kt
@@ -2,8 +2,8 @@ package astminer.parse.javaparser
 
 import astminer.common.model.Parser
 import astminer.parse.ParsingException
+import com.github.javaparser.JavaParser
 import com.github.javaparser.ParseProblemException
-import com.github.javaparser.StaticJavaParser
 import mu.KotlinLogging
 import java.io.InputStream
 
@@ -12,9 +12,11 @@ private val logger = KotlinLogging.logger("JavaParser")
 class JavaParserParseWrapper : Parser<JavaParserNode> {
     override fun parseInputStream(content: InputStream): JavaParserNode {
         try {
-            val tree = StaticJavaParser.parse(content)
+            val tree = JavaParser().parse(content).result.get()
             return JavaParserNode(tree, null)
         } catch (e: ParseProblemException) {
+            throw ParsingException(parserType = "JavaParser", language = "Java", exc = e)
+        } catch (e: NoSuchElementException) {
             throw ParsingException(parserType = "JavaParser", language = "Java", exc = e)
         }
     }
