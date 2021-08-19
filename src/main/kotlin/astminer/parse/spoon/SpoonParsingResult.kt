9203a81431887151d3e3bc770d651ee1022d2eb6
illided <utkin.ilya.nickolaevich@gmail.com>
2021-08-19 23:10:05 +0300

    now it compiles

--- src/main/kotlin/astminer/parse/spoon/SpoonParsingResult.kt
+++ src/main/kotlin/astminer/parse/spoon/SpoonParsingResult.kt
@@ -3,11 +3,12 @@ package astminer.parse.spoon
 import astminer.common.model.*
 import java.io.File
 
-class SpoonParsingResult(file: File) : ParsingResult<SpoonNode>(file) {
+class SpoonParsingResult(file: File, inputDirectoryPath: String?) : ParsingResult<SpoonNode>(file, inputDirectoryPath) {
     override val root: SpoonNode = SpoonJavaParser().parseFile(file)
     override val splitter: TreeFunctionSplitter<SpoonNode> = SpoonJavaFunctionSplitter()
 }
 
 class SpoonParsingResultFactory : ParsingResultFactory {
-    override fun parse(file: File): ParsingResult<out Node> = SpoonParsingResult(file)
+    override fun parse(file: File, inputDirectoryPath: String?): ParsingResult<out Node> =
+        SpoonParsingResult(file, inputDirectoryPath)
 }
