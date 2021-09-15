29eb5eae2cf13f247c5634ae2b34f20b0755257a
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-09-15 17:11:54 +0300

    now it compiles

--- src/main/kotlin/astminer/parse/javalang/JavaLangParser.kt
+++ src/main/kotlin/astminer/parse/javalang/JavaLangParser.kt
@@ -1,33 +1,15 @@
 package astminer.parse.javalang
 
-import astminer.common.model.Parser
 import astminer.config.FileExtension
 import astminer.config.ParserType
 import astminer.parse.ForeignParser
-import astminer.parse.ParsingException
-import astminer.parse.SimpleNode
-import astminer.parse.getTreeFromPythonScript
-import kotlinx.serialization.SerializationException
-import org.apache.commons.io.FileUtils.copyInputStreamToFile
 import java.io.File
-import java.io.InputStream
-import kotlin.io.path.createTempDirectory
-import kotlin.io.path.createTempFile
 
 class JavaLangParser : ForeignParser() {
-    override fun getArguments(file: File): Array<out String> {
-        return arrayOf("python3", "src/main/python/parse/javalang/main.py", "-f", file.path, "&")
+    override fun getArguments(file: File): List<String> {
+        return listOf("python3", "src/main/python/parse/javalang/main.py", "-f", file.path, "&")
     }
 
     override val parser: ParserType = ParserType.JavaLang
     override val language: FileExtension = FileExtension.Java
-
-//    override fun parseFile(file: File): SimpleNode {
-//        return try {getTreeFromPythonScript("python3","src/main/python/parse/javalang/main.py","-f", file.path, "&")}
-//        catch (e: SerializationException) {throw ParsingException(parserType = "JavaLang", language = "java", e)}
-//    }
-//
-//    override fun parseInputStream(content: InputStream): SimpleNode {
-//        throw ParsingException(parserType = "JavaLang", language = "java")
-//    }
 }
