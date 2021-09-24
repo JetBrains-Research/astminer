cca57f5c5701a5e670290ae32e1fbd1d8b2c7e30
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-09-24 16:02:57 +0300

    return type extraction

--- src/main/kotlin/astminer/parse/treesitter/java/TreeSitterJavaFunctionInfo.kt
+++ src/main/kotlin/astminer/parse/treesitter/java/TreeSitterJavaFunctionInfo.kt
@@ -1,7 +1,11 @@
 package astminer.parse.treesitter.java
 
+import astminer.common.EMPTY_TOKEN
+import astminer.common.model.EnclosingElement
 import astminer.common.model.FunctionInfo
+import astminer.common.model.FunctionInfoParameter
 import astminer.common.model.SimpleNode
+import astminer.parse.antlr.getTokensFromSubtree
 
 class TreeSitterJavaFunctionInfo(override val root: SimpleNode, override val filePath: String) :
     FunctionInfo<SimpleNode> {
@@ -9,5 +13,41 @@ class TreeSitterJavaFunctionInfo(override val root: SimpleNode, override val fil
 
     companion object {
         const val FUNCTION_NAME = "identifier"
+        const val BODY = "block"
+        const val ARRAY_TYPE = "array_type"
+        val returnTypes = listOf(
+            "void_type",
+            "integral_type",
+            "floating_point_type",
+            "boolean_type",
+            "type_identifier",
+            "scoped_type_identifier",
+            "generic_type",
+            ARRAY_TYPE
+        )
     }
+
+    override val body: SimpleNode? = root.getChildOfType(BODY)
+
+    override val annotations: List<String>?
+        get() = super.annotations
+    override val modifiers: List<String>?
+        get() = super.modifiers
+    override val parameters: List<FunctionInfoParameter>?
+        get() = super.parameters
+
+    override val returnType: String = run {
+        val returnTypeNode = root.children.find { returnTypes.contains(it.typeLabel) }
+        checkNotNull(returnTypeNode) { "No return type in function $name in file $filePath" }
+
+        var collectedType = returnTypeNode.getTokensFromSubtree()
+        if (returnTypeNode.typeLabel == ARRAY_TYPE) {
+            collectedType = collectedType.replace(EMPTY_TOKEN, "[]") }
+        return@run collectedType
+    }
+
+    override val enclosingElement: EnclosingElement<SimpleNode>?
+        get() = super.enclosingElement
+    override val isConstructor: Boolean
+        get() = super.isConstructor
 }
