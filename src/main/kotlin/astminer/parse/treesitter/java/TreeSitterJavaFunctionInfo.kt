d51c015db73d498b6c9aae9449d1b88f275ed92c
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-09-25 17:50:13 +0300

    detekt issues

--- src/main/kotlin/astminer/parse/treesitter/java/TreeSitterJavaFunctionInfo.kt
+++ src/main/kotlin/astminer/parse/treesitter/java/TreeSitterJavaFunctionInfo.kt
@@ -11,6 +11,32 @@ class TreeSitterJavaFunctionInfo(override val root: SimpleNode, override val fil
     FunctionInfo<SimpleNode> {
     override val nameNode: SimpleNode? = root.getChildOfType(FUNCTION_NAME)
 
+    override val body: SimpleNode? = root.getChildOfType(BODY)
+
+    override val annotations: List<String>?
+        get() = super.annotations
+
+    override val modifiers: List<String>?
+        get() = super.modifiers
+    override val parameters: List<FunctionInfoParameter>?
+        get() = super.parameters
+    override val returnType: String = run {
+        val returnTypeNode = root.children.find { returnTypes.contains(it.typeLabel) }
+        checkNotNull(returnTypeNode) { "No return type in function $name in file $filePath" }
+
+        var collectedType = returnTypeNode.getTokensFromSubtree()
+        if (returnTypeNode.typeLabel == ARRAY_TYPE) {
+            collectedType = collectedType.replace(EMPTY_TOKEN, "[]")
+        }
+        return@run collectedType
+    }
+
+    override val enclosingElement: EnclosingElement<SimpleNode>?
+        get() = super.enclosingElement
+
+    override val isConstructor: Boolean
+        get() = super.isConstructor
+
     companion object {
         const val FUNCTION_NAME = "identifier"
         const val BODY = "block"
@@ -26,28 +52,4 @@ class TreeSitterJavaFunctionInfo(override val root: SimpleNode, override val fil
             ARRAY_TYPE
         )
     }
-
-    override val body: SimpleNode? = root.getChildOfType(BODY)
-
-    override val annotations: List<String>?
-        get() = super.annotations
-    override val modifiers: List<String>?
-        get() = super.modifiers
-    override val parameters: List<FunctionInfoParameter>?
-        get() = super.parameters
-
-    override val returnType: String = run {
-        val returnTypeNode = root.children.find { returnTypes.contains(it.typeLabel) }
-        checkNotNull(returnTypeNode) { "No return type in function $name in file $filePath" }
-
-        var collectedType = returnTypeNode.getTokensFromSubtree()
-        if (returnTypeNode.typeLabel == ARRAY_TYPE) {
-            collectedType = collectedType.replace(EMPTY_TOKEN, "[]") }
-        return@run collectedType
-    }
-
-    override val enclosingElement: EnclosingElement<SimpleNode>?
-        get() = super.enclosingElement
-    override val isConstructor: Boolean
-        get() = super.isConstructor
 }
