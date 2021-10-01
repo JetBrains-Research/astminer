091041144cc79e9d6642ae5e68a704607375ca66
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-10-01 15:25:00 +0300

    function info prototype and tests added

--- src/main/kotlin/astminer/parse/javalang/JavaLangFunctionInfo.kt
+++ src/main/kotlin/astminer/parse/javalang/JavaLangFunctionInfo.kt
@@ -1,10 +1,28 @@
 package astminer.parse.javalang
 
+import astminer.common.model.EnclosingElement
 import astminer.common.model.FunctionInfo
+import astminer.common.model.FunctionInfoParameter
 import astminer.common.model.SimpleNode
 
 class JavaLangFunctionInfo(override val root: SimpleNode, override val filePath: String) : FunctionInfo<SimpleNode> {
     override val nameNode: SimpleNode? = root.getChildOfType(NAME)
+
+    override val body: SimpleNode?
+        get() = super.body
+
+    override val annotations: List<String>?
+        get() = super.annotations
+    override val modifiers: List<String>?
+        get() = super.modifiers
+    override val parameters: List<FunctionInfoParameter>?
+        get() = super.parameters
+    override val returnType: String?
+        get() = super.returnType
+    override val enclosingElement: EnclosingElement<SimpleNode>?
+        get() = super.enclosingElement
+    override val isConstructor: Boolean
+        get() = super.isConstructor
 
     companion object {
         const val NAME = "name"
