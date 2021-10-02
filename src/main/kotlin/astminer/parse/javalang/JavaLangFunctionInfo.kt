f619df56268f6a410ecb9ceb0dea68a6dc92f4ba
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-10-02 15:31:58 +0300

    return type and body extraction

--- src/main/kotlin/astminer/parse/javalang/JavaLangFunctionInfo.kt
+++ src/main/kotlin/astminer/parse/javalang/JavaLangFunctionInfo.kt
@@ -8,8 +8,7 @@ import astminer.common.model.SimpleNode
 class JavaLangFunctionInfo(override val root: SimpleNode, override val filePath: String) : FunctionInfo<SimpleNode> {
     override val nameNode: SimpleNode? = root.getChildOfType(NAME)
 
-    override val body: SimpleNode?
-        get() = super.body
+    override val body: SimpleNode? = root.getChildOfType(BODY)
 
     override val annotations: List<String>?
         get() = super.annotations
@@ -17,14 +16,26 @@ class JavaLangFunctionInfo(override val root: SimpleNode, override val filePath:
         get() = super.modifiers
     override val parameters: List<FunctionInfoParameter>?
         get() = super.parameters
-    override val returnType: String?
-        get() = super.returnType
+
+    override val returnType: String = root.children.find { it.typeLabel in possibleTypes }?.extractType() ?: VOID
+
     override val enclosingElement: EnclosingElement<SimpleNode>?
         get() = super.enclosingElement
     override val isConstructor: Boolean
         get() = super.isConstructor
 
+    private fun SimpleNode.extractType(): String = this.preOrder()
+            .mapNotNull { if (it.typeLabel == "dimensions" && it.isLeaf()) "[]" else it.originalToken }
+            .joinToString(separator = "")
+
     companion object {
         const val NAME = "name"
+        const val BODY = "body"
+
+        const val VOID = "void"
+        val possibleTypes = listOf(
+            "BasicType",
+            "ReferenceType"
+        )
     }
 }
