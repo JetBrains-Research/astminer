02c213c1e9762cb72899a66e994ce1e19ac1331d
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-10-02 12:30:45 +0300

    python scripts refactored

--- src/main/python/parse/tree_sitter/main.py
+++ src/main/python/parse/tree_sitter/main.py
@@ -3,11 +3,14 @@ import json
 from argparse import ArgumentParser
 import os
 from typing import List
-from ast import TreeBuilder
+from ast import TreeBuilder, TreeAsDict
 
 argument_parser = ArgumentParser()
-argument_parser.add_argument("-f", "--file", dest="filename", type=str, help="path to the target file", metavar="FILE")
-argument_parser.add_argument("-b", "--build", dest="grammars", nargs="+")
+argument_parser.add_argument("-f", "--file", dest="filename", type=str, help="Path to the target file", metavar="FILE")
+argument_parser.add_argument(
+    "-b", "--build", dest="grammars", nargs="+", help="Path to grammar folders. For example: -b tree-sitter-java"
+)
+argument_parser.add_argument("-l", "--language", dest="language", help="Target language. For example: -l java")
 
 
 def build_libraries(languages: List[str]):
@@ -19,27 +22,23 @@ def build_libraries(languages: List[str]):
     )
 
 
-def setup_java_parser() -> Parser:
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, "build/my-languages.so")
-    java_grammar = Language(filename, "java")
-    java_parser = Parser()
-    java_parser.set_language(java_grammar)
-    return java_parser
+class TreeSitterLauncher:
+    _parser: Parser
 
+    def __init__(self, language, library_path):
+        grammar = Language(library_path, language)
+        self._parser = Parser()
+        self._parser.set_language(grammar)
 
-def get_code_bytes(filepath: str) -> bytes:
-    example = open(filepath, "r")
-    return bytes(example.read(), "utf-8")
+    def _get_code_bytes(self, filepath: str) -> bytes:
+        file = open(filepath, "r")
+        return bytes(file.read(), "utf-8")
 
-
-def parse_file(filename: str):
-    parser = setup_java_parser()
-    code_bytes = get_code_bytes(filename)
-    tree = parser.parse(code_bytes)
-    cursor = tree.walk()
-    tree_as_dict = TreeBuilder(cursor, code_bytes).get_tree_as_dict()
-    print(json.dumps(tree_as_dict))
+    def parse_file(self, filepath: str) -> TreeAsDict:
+        code_bytes = self._get_code_bytes(filepath)
+        tree_sitter_tree = self._parser.parse(code_bytes)
+        cursor = tree_sitter_tree.walk()
+        return TreeBuilder(cursor, code_bytes).get_tree_as_dict()
 
 
 def main():
@@ -49,7 +48,10 @@ def main():
         build_libraries(args.grammars)
 
     if args.filename is not None:
-        parse_file(args.filename)
+        dirname = os.path.dirname(__file__)
+        library_path = os.path.join(dirname, "build/my-languages.so")
+        tree = TreeSitterLauncher(args.language, library_path).parse_file(args.filename)
+        print(json.dumps(tree))
 
 
 if __name__ == "__main__":
