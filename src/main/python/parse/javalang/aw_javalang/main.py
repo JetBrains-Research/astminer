f805fc751d3060948d30a3d7613029c7e5adf436
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-10-20 16:20:37 +0300

    argument extraction refactor

--- src/main/python/parse/javalang/aw_javalang/main.py
+++ src/main/python/parse/javalang/aw_javalang/main.py
@@ -5,27 +5,31 @@ from dataclasses import asdict
 import json
 import javalang
 
-parser = ArgumentParser()
-parser.add_argument("-f", "--file", dest="filename", type=str, help="path to the target file", metavar="FILE")
-parser.add_argument("-d", "--declined", dest="show_declined", type=bool, help="Show declined attributes", default=False)
 
-
-def main():
-    args, unknown = parser.parse_known_args()
-
-    if args.show_declined:
+def print_json(filename: str, show_declined: bool):
+    if show_declined:
         print("Declined attributes:")
 
-    java_file = open(args.filename, "r").read()
+    java_file = open(filename, "r").read()
     tree = javalang.parse.parse(java_file)
-    AST = generate_presentable_AST(tree, show_declined=args.show_declined)
+    AST = generate_presentable_AST(tree, show_declined=show_declined)
 
-    if args.show_declined:
+    if show_declined:
         print("---------------------------------------------------------------------------")
         print("Generated AST:")
 
     tree_dict = asdict(TreeSerializer().get_enumerated_tree(AST))
     print(json.dumps(tree_dict))
+
+
+# main entrypoint
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename", type=str, help="path to the target file", metavar="FILE")
+    parser.add_argument("-d", "--declined", dest="show_declined", type=bool, help="Show declined attributes",
+                        default=False)
+    args, unknown = parser.parse_known_args()
+    print_json(args.filename, args.show_declined)
 
 
 if __name__ == "__main__":
