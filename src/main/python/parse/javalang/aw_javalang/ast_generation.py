e2b97501d43962400493b86520f1e1a0d9b905a5
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-11-02 17:08:26 +0300

    range support added

--- src/main/python/parse/javalang/aw_javalang/ast_generation.py
+++ src/main/python/parse/javalang/aw_javalang/ast_generation.py
@@ -7,10 +7,27 @@ DUMMY_NONE_PROCESSING_IN_ITERABLE = True
 
 
 @dataclass
+class Position:
+    l: int
+    c: int
+
+
+@dataclass
+class Range:
+    start: Position
+    end: Position
+
+
+def node_range(start: List[int], end: List[int]):
+    return Range(Position(start[0], start[1]), Position(end[0], end[1]))
+
+
+@dataclass
 class Node:
     type: str
     value: Optional[str]
     children: List["Node"]
+    node_range: Range
 
     def __str__(self):
         return self.type + (f" : {self.value}" if self.value is not None else "")
@@ -38,7 +55,7 @@ def generate_presentable_AST(node: JavaLangNode, show_declined: bool = True) -> 
         if isinstance(value, str):
             children.append(process_string_attribute(node, attr, value))
         elif isinstance(value, list) or isinstance(value, set):
-            attribute_node = Node(attr, None, [])
+            attribute_node = Node(attr, None, [], node_range([-1, -1], [-1, -1]))
             attribute_node.children = process_iterable_attributes(attribute_node, attr, value, show_declined)
             if not attribute_node.is_leaf():
                 children.append(attribute_node)
@@ -46,7 +63,13 @@ def generate_presentable_AST(node: JavaLangNode, show_declined: bool = True) -> 
             children.append(process_node_attribute(node, value, show_declined))
         elif (value is not None or (value is None and not IGNORE_NONE_ATTR)) and show_declined:
             process_declined_attribute(attr, value)
-    return Node(generate_node_type(node), None, children)
+    node_pos = node.position
+    if node_pos is None:
+        start = [-1, -1]
+    else:
+        start = [node_pos.line, node_pos.column]
+    return Node(type=generate_node_type(node), value=None, children=children,
+                node_range=node_range(start, [-1, -1]))
 
 
 def generate_node_type(node: JavaLangNode) -> str:
@@ -72,7 +95,7 @@ def get_singular(string: str) -> str:
 
 
 def process_string_attribute(node: Node, attr: str, value: str) -> Node:
-    return Node(attr, value, [])
+    return Node(attr, value, [], node_range([-1, -1], [-1, -1]))
 
 
 def process_node_attribute(node: Node, value: JavaLangNode, show_declined: bool) -> Node:
--- src/main/python/parse/javalang/aw_javalang/tree_flattening.py
+++ src/main/python/parse/javalang/aw_javalang/tree_flattening.py
@@ -1,5 +1,5 @@
 from typing import Optional, List, Tuple
-from aw_javalang.ast_generation import Node
+from aw_javalang.ast_generation import Node, Range
 from dataclasses import dataclass
 
 
@@ -13,14 +13,14 @@ class EnumeratedNode:
     token: Optional[str]
     nodeType: str
     children: List[int]
-
+    range: Range
 
 class TreeSerializer:
     def __init__(self):
         self._current_id = 0
 
     def _enumerate_tree(self, node) -> Tuple[List["EnumeratedNode"], int]:
-        enumerated_root = EnumeratedNode(node.value, node.type, [])
+        enumerated_root = EnumeratedNode(node.value, node.type, [], node.node_range)
         root_id = self._current_id
         self._current_id += 1
         enumerated_tree = [enumerated_root]
